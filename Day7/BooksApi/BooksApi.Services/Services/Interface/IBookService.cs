6844badb1bbec8b77f68720e9e7bc79299bbda9d
shrey patel <shreyp683@gmail.com>
2025-06-04 11:01:34 +0530

    Data retrieval using joins in the LINQ Query, Sorting, filtering and paging

---
+++ Day7/BooksApi/BooksApi.Services/Services/Interface/IBookService.cs
@@ -0,0 +1,16 @@
+﻿using BooksApi.Entities.Entities;
+using BooksApi.Models;
+
+namespace BooksApi.Services.Services.Interface
+{
+    public interface IBookService
+    {
+        void AddBook(Book book);
+        List<Book> GetAll();
+        Book? GetBookById(int id);
+        Task InsertBook(Book book);
+        BookDetails GetBookDetailsById(int id);
+        Task<List<Book>> GetAllAsync(); // Added async method for fetching all books
+        Task GetAllBooks();
+    }
+}
---
+++ Day7/BooksApi/BooksApi/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using BooksApi.Data.Models;
+using BooksApi.Services;
+using BooksApi.Services.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+        private readonly IBookService _bookService; // Added book service
+
+        public UserController(IUserService userService, IBookService bookService) // Inject book service
+        {
+            _userService = userService;
+            _bookService = bookService;
+        }
+
+        [HttpPost]
+        [Route("all")]
+        public async Task<IActionResult> GetAllUsers([FromBody] FilterVM filterRequest)
+        {
+            var data = await _userService.GetAllUsers(filterRequest);
+            return Ok(data);
+        }
+
+        // Added: Get all books endpoint
+        [HttpGet]
+        [Route("books")]
+        public async Task<IActionResult> GetAllBooks()
+        {
+            var books = await _bookService.GetAllBooks();
+            return Ok(books);
+        }
+    }
+}
